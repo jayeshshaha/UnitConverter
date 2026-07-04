b6f4fa97a5ac612b06e75cbc5a5153dffa943c3a
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-07-04 14:38:59 +0530

    integrated swagger

---
+++ UnitConverter.API/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi;
+
+namespace UnitConverter.API.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerExtension(this IServiceCollection services)
+        {
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Unit Converter API",
+                    Version = "v1",
+                    Description = "A RESTful API for converting units."
+                });
+            });
+
+            return services;
+        }
+    }
+}
--- UnitConverter.API/Program.cs
+++ UnitConverter.API/Program.cs
@@ -1,4 +1,6 @@
 
+using UnitConverter.API.Extensions;
+
 namespace UnitConverter.API
 {
     public class Program
@@ -8,24 +10,24 @@ namespace UnitConverter.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddSwaggerExtension();
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-
+   
             app.MapControllers();
 
             app.Run();
